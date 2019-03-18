a4df0d2e0bc8fbb0ecf1f74908dc35c2ca51dba4
Colin Jaffe <balloonasaurus@gmail.com>
2019-03-18 11:15:25 -0400

    completes solution

--- main.js
+++ main.js
@@ -16,12 +16,17 @@ window.onload = init;
 // Set up all event listeners.
 function init() {
     // When they click the add todo button, run `addTodo`.
-
-
-    // When they click the clear done todos button, run `clearDoneTodos`.
-
-
-    // When they click the clear all todos button, run `clearAllTodos`.
+    document.querySelector('#add-todo')
+        .addEventListener('click', addTodo);
+        
+        // When they click the clear done todos button, run `clearDoneTodos`.
+    document.querySelector('#clear-done-todos')
+        .addEventListener('click', clearDoneTodos);
+        
+        
+        // When they click the clear all todos button, run `clearAllTodos`.
+    document.querySelector('#clear-all-todos')
+        .addEventListener('click', clearAllTodos);
 
 }
 
@@ -30,69 +35,148 @@ function addTodo(event) {
     event.preventDefault();
 
     // Get new todo from the new todo input field.
+    const newTodo = document.querySelector('#new-todo').value;
 
+    // Clear the input field of all text.
+    document.querySelector('#new-todo').value = '';
 
     // Put the todo and its "done-ness" in their respective arrays.
+    todos.push(newTodo);
+    isDone.push(false);
 
-
-    // Add the todo to the html.
-
-
+    // Create a new html element and put our new todo's text in there.
+    const newLi = document.createElement('li');
+    newLi.innerText = newTodo;
+    
     // Add an event listener on the newly created html element to launch
-    // `markDone` when it's clicked.
+    // `toggleDone` when it's clicked.
+    newLi.addEventListener('click', toggleDone);
 
+    // Put our new element on the list part of our page!
+    const ol = document.querySelector('#todo-list');
+    ol.appendChild(newLi);
 }
 
-function clearDoneTodos() {
+
+function clearAllTodos(event) {
+    // Stop page from reloading on button click.
+    event.preventDefault();
+    
+    // Remove all todos from BOTH arrays.
+    todos.splice(0);
+    isDone.splice(0);
+    
+    // Remove all todos from the html.
+    removeAllChildrenOfOl();
+}
+
+function clearDoneTodos(event) {
     // Stop page from reloading on button click.
     event.preventDefault();
 
     /*
         Find which todos need to be removed and remove them from BOTH arrays.
         If you did it right when making them, you should be able to check the
-        `isDone` array to figure this out.
+        `isDone` array to figure out which ones are, in fact, done. Remember
+        that there is a 1:1 relationship between `todos` indices and `isDone`
+        indices!
 
         One way to do this is to build up a new array. Give that a try first!
 
     */
+   console.log('before')
 
+   console.log(`todos: ${todos}`)
+   console.log(`isDone: ${isDone}`);
+
+    const notDone = [];
+    const newIsDone = [];
+
+    for(let i = 0; i < todos.length; i++) {
+        if(!isDone[i]) {
+            notDone.push(todos[i]);
+            // isDone.splice(i, 1);
+            newIsDone.push(false);
+        }
+    }
+
+    todos = notDone;
+    isDone = newIsDone;
+
+    console.log('after')
+
+    console.log(`todos: ${todos}`)
+    console.log(`isDone: ${isDone}`);
 
     /*
         Now remove the done todos from the html.
 
-        There are (at least) two ways to do this: find which ones have the
-        strikethrough style by looping through them (difficult but hardly
-        impossible), or remove the correct children when doing the array removal
-        step above (way easier).
+        Although it's not technically efficient as there is a slight time cost
+        to rendering new elements on a web page, you might think not of removing
+        certain todos but making a new set of lis to replace what we have. You
+        may even already have some code to clear the whole list!
+
+        You could do it the harder but more html efficient way instead, though.
+
+        Your call.
     */
 
-
-}
-
-function clearAllTodos(event) {
-    // Stop page from reloading on button click.
-    event.preventDefault();
-
-    // Remove all todos from BOTH arrays.
-
-
-    // Remove all todos from the html.
     removeAllChildrenOfOl();
+
+    for(let i = 0; i < todos.length; i++) {
+        const newLi = document.createElement('li');
+        newLi.innerText = todos[i];
+
+        newLi.addEventListener('click', toggleDone);
+
+        const ol = document.querySelector('#todo-list');
+        ol.appendChild(newLi);
+    }
 }
 
-function markDone(event) {
+function toggleDone(event) {
     // No need to run `event.preventDefault` here; that default behavior only
     // applies to buttons.
-
+    
     // Grab the HTML element that was clicked.
-    // You may have to research how it's done, but the `event` parameter you
-    // were passed is a good place to start!
+    // If you don't know, the event parameter has what you need... somewhere.
+    const li = event.target;
 
+    // Find the index of the array that this todo resides in. There are a couple
+    // ways to do this, and I'm sure you'll figure one out!
 
-    // Apply 'strikethrough' styling to it.
+    const ol = document.querySelector('#todo-list');
+    const lis = ol.childNodes;
+    console.dir(`lis:`, lis)
+    let liIndex = -1;
+    
+    for(let i = 0; i < lis.length; i++) {
+        if(lis[i].innerText === li.innerText) {
+            liIndex = i;
+        }
+    }
 
+    // *IF* it's not done yet, apply strikethrough. Otherwise, take it away!
+    if(isDone[liIndex]) {
+        li.style.textDecoration = '';
+    } else {
+        li.style.textDecoration = 'line-through';
+    }
 
-    // Mark done the same todo in the array, using the isDone array.
-    // There are a couple different ways to find the appropraite index. I bet
-    // you'll figure one out!
+    // Toggle the "done-ness" of the same todo, using the isDone array.
+    isDone[liIndex] = !isDone[liIndex];
+}
+
+function removeAllChildrenOfOl() {
+    // Grab the ol.
+    const ol = document.querySelector('#todo-list');
+
+    // Remove all its children.
+    // The way I like to do that is to continue to remove children as long as
+    // there are some to remove.
+    // Look at the methods `.hasChildNodes` and `removeChild`.
+    // There are other ways too, though. Feel free to poke around.
+    while(ol.hasChildNodes()) {
+        ol.removeChild(ol.firstChild);
+    }
 }
