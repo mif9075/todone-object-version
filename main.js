f5e1e5289c8b8ce1b46992a962dc342653c848f4
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-03-26 15:32:56 -0400

    Remove extra comments

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-/*
-
-Global arrays. There ought to be a 1:1 relationship of each index of your todos
-and each index of your isDone.
-
-For example, isDone[3] would hold the "done-ness" information for todos[3].
-
-*/
-
 // let objectDos = {todos : [], isDone : []};
 let todos = [];
 
@@ -51,59 +42,27 @@ function addTodo(event) {
 
 
 function clearAllTodos(event) {
-    // Stop page from reloading on button click.
+    
     event.preventDefault();
     
-    // Remove all todos from BOTH arrays.
     todos.splice(0);
     
-    // Remove all todos from the html.
     removeAllChildrenOfOl();
 }
 
 function clearDoneTodos(event) {
-    // Stop page from reloading on button click.
+
     event.preventDefault();
 
-    /*
-        Find which todos need to be removed and remove them from BOTH arrays.
-        If you did it right when making them, you should be able to check the
-        `isDone` array to figure out which ones are, in fact, done. Remember
-        that there is a 1:1 relationship between `todos` indices and `isDone`
-        indices!
-
-        One way to do this is to build up a new array. Give that a try first!
-
-    */
-
     const notDone = [];
-    // const newIsDone = [];
 
     for(let i = 0; i < todos.length; i++) {
         if(!todos[i].isDone) {
-            // notDone.push(todos[i]);
-            // isDone.splice(i, 1);
-            // newIsDone.push(false);
             notDone.push(todos[i]);
-            // console.log(notDone);
         }
     }
 
     todos = notDone;
-    // isDone = newIsDone;
-
-    /*
-        Now remove the done todos from the html.
-
-        Although it's not technically efficient as there is a slight time cost
-        to rendering new elements on a web page, you might think not of removing
-        certain todos but making a new set of lis to replace what we have. You
-        may even already have some code to clear the whole list!
-
-        You could do it the harder but more html efficient way instead, though.
-
-        Your call.
-    */
 
     removeAllChildrenOfOl();
 
@@ -119,15 +78,8 @@ function clearDoneTodos(event) {
 }
 
 function toggleDone(event) {
-    // No need to run `event.preventDefault` here; that default behavior only
-    // applies to buttons.
-    
-    // Grab the HTML element that was clicked.
-    // If you don't know, the event parameter has what you need... somewhere.
-    const li = event.target;
 
-    // Find the index of the array that this todo resides in. There are a couple
-    // ways to do this, and I'm sure you'll figure one out!
+    const li = event.target;
 
     const ol = document.querySelector('#todo-list');
     const lis = ol.childNodes;
@@ -139,31 +91,23 @@ function toggleDone(event) {
         }
     }
 
-    // *IF* it's not done yet, apply strikethrough. Otherwise, take it away!
     if(todos[liIndex].isDone) {
         li.style.textDecoration = '';
     } else {
         li.style.textDecoration = 'line-through';
     }
 
-    // Toggle the "done-ness" of the same todo, using the isDone array.
     todos[liIndex].isDone = !todos[liIndex].isDone;
 }
 
 function removeAllChildrenOfOl() {
-    // Grab the ol.
+
     const ol = document.querySelector('#todo-list');
 
-    // Remove all its children.
-    // The way I like to do that is to continue to remove children as long as
-    // there are some to remove.
-    // Look at the methods `.hasChildNodes` and `removeChild`.
-    // There are other ways too, though. Feel free to poke around.
     while(ol.hasChildNodes()) {
         ol.removeChild(ol.firstChild);
     }
 }
-
 
 //Helper Functions
 
